455c5b1c86e9e1f67d506ac0df5a3246d9a6f1b8
jgsheppa <james.sheppard@avisio.ai>
2022-04-08 18:24:23 +0200

    Improve search suggestions

--- models/suggestions.go
+++ models/suggestions.go
@@ -9,36 +9,56 @@ func CreateSuggestions(article Article) []redisearch.Suggestion {
 	var suggestion []redisearch.Suggestion
 
 	splitTitle := strings.Split(article.Title, " ")
-
-	for _, word := range splitTitle {
-		suggestion = append(suggestion, redisearch.Suggestion{
-			Term:    word,
-			Score:   100,
-			Payload: word,
-			Incr:    false,
-		})
-	}
+	title := SuggestionFactory(splitTitle)
+	suggestion = append(suggestion, title...)
 
 	splitAuthor := strings.Split(article.Author, " ")
-
-	for _, word := range splitAuthor {
-		suggestion = append(suggestion, redisearch.Suggestion{
-			Term:    word,
-			Score:   100,
-			Payload: word,
-			Incr:    false,
-		})
-	}
+	author := SuggestionFactory(splitAuthor)
+	suggestion = append(suggestion, author...)
 
 	splitTopic := strings.Split(article.Topic, " ")
+	topic := SuggestionFactory(splitTopic)
+	suggestion = append(suggestion, topic...)
 
-	for _, word := range splitTopic {
+	return suggestion
+}
+
+func SuggestionFactory(wordArray []string) []redisearch.Suggestion {
+	var suggestion []redisearch.Suggestion
+
+	for index, word := range wordArray {
+		wordArrayLength := len(wordArray) - 1
+		increment := 2
+		stopSlice := increment + index
+
+		if index == wordArrayLength {
+			return suggestion
+		}
+		stringPortion := strings.Join(wordArray[0:stopSlice], " ")
+
+		suggestion = append(suggestion, redisearch.Suggestion{
+			Term:    stringPortion,
+			Score:   100,
+			Payload: stringPortion,
+			Incr:    false,
+		})
+
+		stringPortion = strings.Join(wordArray[index:stopSlice], " ")
+
+		suggestion = append(suggestion, redisearch.Suggestion{
+			Term:    stringPortion,
+			Score:   100,
+			Payload: stringPortion,
+			Incr:    false,
+		})
+
 		suggestion = append(suggestion, redisearch.Suggestion{
 			Term:    word,
 			Score:   100,
 			Payload: word,
 			Incr:    false,
 		})
+
 	}
 
 	return suggestion

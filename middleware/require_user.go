abdead6d5e33213c496a5bd4a941a963338e2927
jgsheppa <james.sheppard@avisio.ai>
2022-04-24 00:33:01 +0200

    Check for bearer token in middleware

--- middleware/require_user.go
+++ middleware/require_user.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jgsheppa/search_engine/context"
 	"github.com/jgsheppa/search_engine/models"
 	"net/http"
+	"strings"
 )
 
 type User struct {
@@ -17,21 +18,36 @@ func (mw *User) Apply(next http.Handler) http.HandlerFunc {
 
 func (mw *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// if the user is logged in then pass the user for the navbar
-		cookie, err := r.Cookie("remember_token")
-		if err != nil {
-			json.NewEncoder(w).Encode(models.AuthError)
-			return
-		}
-		user, err := mw.UserService.ByRemember(cookie.Value)
-		if err != nil {
-			json.NewEncoder(w).Encode(models.AuthError)
-			return
-		}
+		auth := r.Header.Get("Authorization")
+		bearer := strings.Split(auth, " ")
+		var token string
 
-		ctx := r.Context()
-		ctx = context.WithUser(ctx, user)
-		r = r.WithContext(ctx)
-		next(w, r)
+		if len(bearer) > 1 {
+			token = bearer[1]
+
+			user, _ := mw.UserService.ByRemember(token)
+
+			ctx := r.Context()
+			ctx = context.WithUser(ctx, user)
+			r = r.WithContext(ctx)
+			next(w, r)
+		} else {
+			// if the user is logged in then pass the user for the navbar
+			cookie, err := r.Cookie("remember_token")
+			if err != nil {
+				json.NewEncoder(w).Encode(models.AuthError)
+				return
+			}
+			user, err := mw.UserService.ByRemember(cookie.Value)
+			if err != nil {
+				json.NewEncoder(w).Encode(models.AuthError)
+				return
+			}
+
+			ctx := r.Context()
+			ctx = context.WithUser(ctx, user)
+			r = r.WithContext(ctx)
+			next(w, r)
+		}
 	}
 }
